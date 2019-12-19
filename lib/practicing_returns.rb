df7b8572cd695749b44da8cf9e3061c34ddf27f0
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:36:05 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,10 @@
 require 'pry'
 
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+def hello(array)
+  i = 0
+  while i < array.length
+    binding.pry
+    yield(array[i])
+    i += 1
+  end
+end
